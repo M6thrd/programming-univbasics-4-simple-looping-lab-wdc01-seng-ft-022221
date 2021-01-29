8320f55334ce915a887d7d94f2772e65a5fc7522
James Hoggard <jayrh3246@gmail.com>
2021-01-29 21:29:04 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -26,11 +26,12 @@ def output_array(array)
 end
 
 def return_string_array(array)
-  counter = 0
-
-  while counter <array.length do
-    p array[counter].to_s
-    counter += 1
+  count = 0
+  new_array = []
+  while count < array.length do
+    new_array.push(array[count].to_s) # OR new_array << array[count].to_s
+    count += 1
   end
+  new_array
 end
 # Write your methods here
